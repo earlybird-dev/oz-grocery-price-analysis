b12c13e86f88cb126a447a6160d699777e250242
Ha Nguyen <trungha.dev@gmail.com>
2024-03-17 14:51:59 +1100

    Update gcp filename

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "creds" {
   description = "The ID of the project where you want to create your resources."
   type        = string
-  default     = "../grocery-price-analysis-d0419829e9b5.json"
+  default     = "../gcp.json"
 }
 
 variable "project_id" {
